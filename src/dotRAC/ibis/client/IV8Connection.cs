284d8416c5e856c4318007253e1a5173c05aa655
yukon39 <yukon39@gmail.com>
2019-07-07 22:32:21 +0200

    Add V8Connection interfaces

---
+++ src/dotRAC/ibis/client/IV8Connection.cs
@@ -0,0 +1,25 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+using System;
+using System.Collections.Generic;
+using dotRAC.ibis.transport;
+
+namespace dotRAC.ibis.client
+{
+    public interface IV8Connection
+    {
+        IV8Transport StartService(string paramString1, List<string> paramList, string paramString2, IV8MessageFormatFactory paramIV8MessageFormatFactory);
+
+        bool IsSame(IV8Connection paramIV8Connection);
+    }
+}
+//public interface IV8Connection extends IV8Lifecycle<IV8ConnectionContext, V8ConnectionException> {
+//    IV8Transport startService(String paramString1, List<String> paramList, String paramString2, IV8MessageFormatFactory paramIV8MessageFormatFactory) throws V8ServiceException;
+
+//    boolean isSame(IV8Connection paramIV8Connection);
+//}
