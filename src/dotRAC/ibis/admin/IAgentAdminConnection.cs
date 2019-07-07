e764c920965cd8976d794baec371de173b269348
yukon39 <yukon39@gmail.com>
2019-07-07 09:51:10 +0200

    Нчальные интерфейсы клиентского подключения

---
+++ src/dotRAC/ibis/admin/IAgentAdminConnection.cs
@@ -0,0 +1,18 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+using System;
+
+namespace dotRAC.ibis.admin
+{
+    public interface IAgentAdminConnection
+    {
+        void AuthenticateAgent(string paramString1, string paramString2);
+        void Authenticate(Guid paramUUID, string paramString1, string paramString2);
+        void AddAuthentication(Guid paramUUID, string paramString1, string paramString2);
+    }
+}
---
+++ src/dotRAC/ibis/admin/client/IAgentAdminConnectorFactory.cs
@@ -0,0 +1,17 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+namespace dotRAC.ibis.admin.client
+{
+    public interface IAgentAdminConnectorFactory
+    {
+        IAgentAdminConnector CreateConnector(uint connectTimeout);
+        
+        //TODO: Add method to interface
+        //IAgentAdminConnector createConnector(Timer paramTimer, Executor paramExecutor, long paramLong);
+    }
+}
