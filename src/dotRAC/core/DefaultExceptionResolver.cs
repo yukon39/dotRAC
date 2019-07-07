694565e76f474d992af162a8460a9cc88a4e07d0
yukon39 <yukon39@gmail.com>
2019-07-07 20:41:29 +0200

    Add core exceptions management

---
+++ src/dotRAC/core/DefaultExceptionResolver.cs
@@ -0,0 +1,37 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+using System;
+using log4net;
+
+namespace dotRAC.core
+{
+    public class DefaultThrowableResolver : IExceptionResolver
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(DefaultThrowableResolver));
+
+        public Exception ResolveException(string name, string message)
+        {
+            Exception exception = default;
+
+            try
+            {
+                Type type = Type.GetType(name);
+                exception = string.IsNullOrEmpty(message) ? (Exception)Activator.CreateInstance(type) : (Exception)Activator.CreateInstance(type, message);
+            }
+            catch (Exception e)
+            {
+                logger.Warn(string.Format("Unresolved exception: ''{0}'' with message: ''{1}''", name, message), e);
+            }
+
+            return exception;
+
+        }
+
+        public string ResolveName(Exception exception) => exception.GetType().Name;
+    }
+}
---
+++ src/dotRAC/core/ThrowableResolverChain.cs
@@ -0,0 +1,39 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+using System;
+using System.Diagnostics.Contracts;
+
+namespace dotRAC.core
+{
+    public class ExceptionResolverChain : IExceptionResolver
+    {
+        private readonly IExceptionResolver first;
+        private readonly IExceptionResolver second;
+
+        public ExceptionResolverChain(IExceptionResolver _first, IExceptionResolver _second)
+        {
+            Contract.Requires(_first != default);
+            Contract.Requires(_second != default);
+
+            first = _first;
+            second = _second;
+        }
+
+        public Exception ResolveException(string name, string message)
+        {
+            Exception exception = first.ResolveException(name, message);
+            return exception == default ? second.ResolveException(name, message) : exception;
+        }
+
+        public string ResolveName(Exception exception)
+        {
+            string name = first.ResolveName(exception);
+            return string.IsNullOrEmpty(name) ? second.ResolveName(exception) : name;
+        }
+    }
+}
