d36a7c9c8c79cde00006dc3c65ba79d1558ef305
yukon39 <yukon39@gmail.com>
2019-07-07 20:42:16 +0200

    Add core security classes

---
+++ src/dotRAC/core/security/SaslProperties.cs
@@ -0,0 +1,47 @@
+﻿/*----------------------------------------------------------
+This Source Code Form is subject to the terms of the 
+Mozilla Public License, v.2.0. If a copy of the MPL 
+was not distributed with this file, You can obtain one 
+at http://mozilla.org/MPL/2.0/.
+----------------------------------------------------------*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotRAC.core.security
+{
+    public class SaslProperties
+    {
+        public readonly string ServerName;
+
+        public string[] Mechanisms
+        {
+            get
+            {
+                string[] result = new string[mechanisms.Count];
+
+                int index = 0;
+                foreach(string key in mechanisms.Keys)
+                {
+                    result[index++] = key;
+                }
+
+                return result;
+            }
+        }
+
+        public Attributes GetAttributes(string name) => mechanisms.First(x => x.Key == name).Value;
+
+        private readonly Dictionary<string, Attributes> mechanisms;
+               
+        public SaslProperties(string serverName)
+        {
+            ServerName = serverName;
+            mechanisms = new Dictionary<string, Attributes>();
+        }
+    }
+}
